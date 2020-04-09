8e1d8b0e78880b679f44d48e30e6a9d8981f2f3d
karosi12 <adekayor@gmail.com>
2020-04-09 01:30:35 +0100

    added login form validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoggerService } from 'src/app/core/services/logger.service';
+import { ValidationService } from 'src/app/core/services/validation.service';
+import { GrowlerService, GrowlerMessageType } from '../core/growler/growler.service';
+import { IUserLogin } from '../shared/interfaces';
 
 @Component({
   selector: 'cm-login',
@@ -7,9 +13,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  errorMessage: string;
+
+  constructor(private formBuilder: FormBuilder,
+              private logger: LoggerService) { }
 
   ngOnInit() {
+      this.buildForm();
+  }
+
+  buildForm() {
+      this.loginForm = this.formBuilder.group({
+          email:      ['', [ Validators.required, ValidationService.emailValidator ]],
+          password:   ['', [ Validators.required, ValidationService.passwordValidator ]]
+      });
+  }
+
+  submit({ value, valid }: { value: IUserLogin, valid: boolean }) {
+    this.logger.log(JSON.stringify(value));
+    return { value, valid };
   }
 
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 
 @NgModule({
-  imports: [ ReactiveFormsModule, LoginRoutingModule ],
+  imports: [ ReactiveFormsModule, CommonModule, LoginRoutingModule ],
   declarations: [ LoginRoutingModule.components ]
 })
 export class LoginModule { }
